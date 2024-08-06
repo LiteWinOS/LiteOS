8bd44908cbf5e7c5655c52c9ac86cd8302aea61d
LiteWinOS <65852844+LiteWinOS@users.noreply.github.com>
2024-08-06 20:09:21 +0300

    Create importpow.bat

---
+++ importpow.bat
@@ -0,0 +1,4 @@
+powercfg /IMPORT c:\LiteOS-Gaming.pow 51f59510-fb3c-42a6-9483-614fd844763d
+powercfg /s 51f59510-fb3c-42a6-9483-614fd844763d
+
+powercfg -changename 51f59510-fb3c-42a6-9483-614fd844763d LiteOS-eSport
