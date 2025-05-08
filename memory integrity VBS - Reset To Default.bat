b063c3d8b16fbece0ebe36fc4b411b85604705eb
LiteWinOS <65852844+LiteWinOS@users.noreply.github.com>
2025-05-08 22:48:42 +0300

    Add files via upload

---
+++ memory integrity VBS - Reset To Default.bat
@@ -0,0 +1,2 @@
+reg delete "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" /f
+reg delete "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard" /v "EnableVirtualizationBasedSecurity" /f
